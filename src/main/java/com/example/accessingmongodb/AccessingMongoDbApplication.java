ef84880c786704b5b205447641bd156d8442d0d4
abhishekverma06 <abhishekverma06@yahoo.com>
2021-09-23 02:11:15 +0530

    Initial Commit

---
+++ src/main/java/com/example/accessingmongodb/AccessingMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingMongoDbApplication.class, args);
+	}
+
+}
